d6fd7cc9d56d5cd5eed288a9cd8ca622ebe575db
Wong Lok <wonglok@users.noreply.github.com>
2023-02-04 22:38:27 +0800

    save

--- src/backend/LoginPage.jsx
+++ src/backend/LoginPage.jsx
@@ -1,39 +1,18 @@
-import { Modal } from '@mantine/core'
-import { useEffect, useState } from 'react'
 import { loginMetamask } from './aws'
 
 export function LoginPage() {
-  let [opened, setOpened] = useState(true)
-
-  useEffect(() => {}, [])
   return (
     <>
-      <Modal
-        opened={opened}
-        closeOnClickOutside={false}
-        withCloseButton={false}
-        closeOnEscape={false}
-        onClose={() => setOpened(false)}
-        title='Login to Admin'>
-        {/* Modal content */}
-
+      <div></div>
+      <div className='flex items-center justify-center w-full h-full'>
         <div>
           <button
             className='p-2 px-5 text-white bg-yellow-500 rounded-xl'
             onClick={() => {
               loginMetamask()
             }}>
-            Metamask
+            Login with Metamask
           </button>
-        </div>
-      </Modal>
-      <div className='flex items-center justify-center w-full h-full'>
-        <div
-          onClick={() => {
-            //
-            setOpened(true)
-          }}>
-          Yo, Let's Login...
         </div>
       </div>
     </>
