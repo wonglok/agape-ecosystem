f0bd5115f396e5bbfca9d9e42219d64625e9e532
Wong Lok <wonglok@users.noreply.github.com>
2023-02-10 23:08:22 +0800

    save

--- src/components/html/PageCodeEditor/CodeEditor.jsx
+++ src/components/html/PageCodeEditor/CodeEditor.jsx
@@ -17,7 +17,7 @@ function AppIcon({
   return (
     <div
       className={
-        'inline-flex flex-col items-center justify-center p-1 px-3 mr-2 transition-all duration-500 ' +
+        'inline-flex flex-col items-center justify-center p-2 px-3 mr-2 transition-all duration-500 ' +
         (st.tabAt === name
           ? ' bg-white border border-gray-300 rounded-lg shadow-inner'
           : ' rounded-lg border border-transparent')
@@ -28,22 +28,22 @@ function AppIcon({
       {icon}
       <div className='text-xs select-none'>{title}</div>
 
-      <div className='mt-1'>
+      <div className=''>
         {st.tabAt === name ? (
           <div
-            className=''
+            className='mt-1'
             style={{
-              width: `3px`,
-              height: `3px`,
+              width: `20px`,
+              height: `1px`,
               backgroundColor: 'lime',
               boxShadow: `0px 0px 5px 0px green`,
             }}></div>
         ) : (
           <div
-            className=''
+            className='mt-1'
             style={{
-              width: `3px`,
-              height: `3px`,
+              width: `20px`,
+              height: `1px`,
             }}></div>
         )}
       </div>
@@ -63,13 +63,12 @@ export function CodeEditor() {
         {/*  */}
         <AppIcon
           name='modulePage'
-          title={'Module'}
+          title={'Store'}
           icon={
             <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>
               <path d='M18.417 6.75v-.947c-.124-1.511-4.042-1.52-4.148-.001h-.013v.948c-.034 1.598 4.214 1.601 4.161 0zm-2.067-1.366c.723 0 1.311.253 1.311.564s-.588.564-1.311.564c-.724 0-1.311-.253-1.311-.564s.587-.564 1.311-.564zm-2.286 3.771v-.947c-.123-1.511-4.042-1.52-4.148-.001h-.013v.948c-.034 1.598 4.213 1.601 4.161 0zm-2.068-.238c-.723 0-1.311-.253-1.311-.564 0-.312.588-.564 1.311-.564.724 0 1.311.253 1.311.564 0 .312-.587.564-1.311.564zm2.068-4.446v-.946c-.124-1.512-4.042-1.52-4.148-.001h-.013v.947c-.034 1.598 4.214 1.602 4.161 0zm-2.067-1.366c.723 0 1.311.253 1.311.564s-.588.564-1.311.564c-.724 0-1.311-.253-1.311-.564s.587-.564 1.311-.564zm.003-3.105l-11 6 .009.019-.009-.005v12.118l11 5.868 11-5.869v-12.055l-11-6.076zm-.003 1.141l9.917 5.458-9.896 5.385-9.903-5.448 9.882-5.395zm9.003 11.63l-8 4.268v-3.039l8-4.353v3.124zm-10 4.268l-8-4.268v-3.172l8 4.401v3.039zm-8-3.228l8 4.268v2.921l-8-4.268v-2.921zm10 4.268l8-4.268v2.921l-8 4.268v-2.921zm-7.447-12.277h-.013v.948c-.034 1.598 4.213 1.601 4.161 0v-.947c-.123-1.511-4.042-1.519-4.148-.001zm2.08.71c-.723 0-1.311-.253-1.311-.564s.588-.565 1.311-.565c.724 0 1.311.253 1.311.564s-.588.565-1.311.565z' />
             </svg>
           }></AppIcon>
-        {/*  */}
       </div>
       <div className='bg-gray-200' style={{ height: `calc(100% - 4.5rem)` }}>
         {tabAt === 'codePage' && <CodeEditorLayout></CodeEditorLayout>}
