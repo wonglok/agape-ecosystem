2f988808bd33ff9c55df86cd91c5348d61317cfb
Wong Lok <wonglok@users.noreply.github.com>
2023-02-20 18:35:48 +0800

    save

--- src/components/html/PageCodeEditor/FlowSystem/useFlow/Edge/CloseEdge/CloseEdge.jsx
+++ src/components/html/PageCodeEditor/FlowSystem/useFlow/Edge/CloseEdge/CloseEdge.jsx
@@ -50,7 +50,16 @@ export default function CustomEdge({
         requiredExtensions='http://www.w3.org/1999/xhtml'>
         <div>
           <button className='edgebutton' onClick={(event) => onEdgeClick(event, id)}>
-            {`×`}
+            <svg
+              className=' scale-50'
+              clipRule='evenodd'
+              fillRule='evenodd'
+              strokeLinejoin='round'
+              strokeMiterlimit='2'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'>
+              <path d='m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z' />
+            </svg>
           </button>
         </div>
       </foreignObject>
@@ -67,7 +76,7 @@ export default function CustomEdge({
   border-radius: 50%;
   font-size: 12px;
   line-height: 1;
-  opacity: 0.1;
+  opacity: 0.05;
   box-shadow: 0 0 6px 2px rgba(0, 0, 0, 0.08);
 }
 
